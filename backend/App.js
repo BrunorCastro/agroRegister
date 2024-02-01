28f371c2e0f9b36cc5f4895833d97bbc35398a60
Manuel Guilherme <m.galmanus@gmail.com>
2024-02-01 14:28:07 -0300

    ...

---
+++ backend/App.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+// const app = express();
+
+// 
+// const admin = require('firebase-admin');
+// const serviceAccount = require('./BackEnd/agro-6ffc8-firebase-adminsdk-hvck8-f1b01e27c9.json');
+
+
+// 
+
+// //app.use(express.json());
+// //app.use(express.urlencoded({ extended: false }));
+
+
+
+// // Importar rotas
+// const nameRoutes = require('./ ');
+// const nameRoutes = require('./ ');
+// const nameRoutes = require('./ ');
+
+
+// // Usar rotas
+// app.use('/api', nameRoutes);
+// app.use('/api', nameRoutes);
+// app.use('/api', nameRoutes);
