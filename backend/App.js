5f46a8782e84ecb2280d1d13d99a994cb1ee0cb7
Manuel Guilherme <m.galmanus@gmail.com>
2024-02-02 17:04:29 -0300

    ...

--- backend/App.js
+++ backend/App.js
@@ -1,34 +1,32 @@
- const express = require('express');       
- const app = express();
-
- 
- const admin = require('firebase-admin');
- const serviceAccount = require('./backend/agro-6ffc8-firebase-adminsdk-hvck8-8366ff3a8e.json');
-
+const express = require("express");
+const app = express();
+const auth = admin.auth();
+const admin = require("firebase-admin");
+const serviceAccount = require("agro-6ffc8-firebase-adminsdk-hvck8-8366ff3a8e.json");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Importar rotas
-const animalRouter = require('./backend/Routes/animalRouter.js ');
-const historico_pesagemRouter = require('./backend/Routes/historico_pesagemRouter.js ');
-const lotesRouter = require('./backend/Routes/lotes ');
-const reproducaoRouter = require ('./backend/Routes/reproducaoRouter.js');
-const saudeRouter = require('./backend/Routes/saudeRouter.js');
-const userRouter = require('./backend/Routes/userRouter.js');
+const animalRouter = require("./backend/Routes/animalRouter.js ");
+const historico_pesagemRouter = require("./backend/Routes/historico_pesagemRouter.js ");
+const lotesRouter = require("./backend/Routes/lotes ");
+const reproducaoRouter = require("./backend/Routes/reproducaoRouter.js");
+const saudeRouter = require("./backend/Routes/saudeRouter.js");
+const userRouter = require("./backend/Routes/userRouter.js");
 
 //usar rotas
-app.use('/api', animalRouter);
-app.use('/api', historico_pesagemRouter);
-app.use('/api', lotesRouter);
-app.use('/api', reproducaoRouter);
-app.use('/api', saudeRouter);
-app.use('/api', userRouter);
+app.use("/api", animalRouter);
+app.use("/api", historico_pesagemRouter);
+app.use("/api", lotesRouter);
+app.use("/api", reproducaoRouter);
+app.use("/api", saudeRouter);
+app.use("/api", userRouter);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 app.listen(port, () => {
-    console.log(`Server running on port ${3000}`);
-  });
-
-
-
-  
+  console.log(`Server running on port ${3000}`);
+});
